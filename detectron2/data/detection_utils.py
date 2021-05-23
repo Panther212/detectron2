22967339187e0c35d5e30c8f4772eadf37c93977
Mukund666 <mukund.choudhary14@gmail.com>
2021-05-23 14:56:28 +0530

    Update detection_utils.py
    to deal with image orientation error while training.

--- detectron2/data/detection_utils.py
+++ detectron2/data/detection_utils.py
@@ -185,24 +185,24 @@ def read_image(file_name, format=None):
         return convert_PIL_to_numpy(image, format)
 
 
-def check_image_size(dataset_dict, image):
+#def check_image_size(dataset_dict, image):
     """
-    Raise an error if the image does not match the size specified in the dict.
+ #   Raise an error if the image does not match the size specified in the dict.
     """
-    if "width" in dataset_dict or "height" in dataset_dict:
-        image_wh = (image.shape[1], image.shape[0])
-        expected_wh = (dataset_dict["width"], dataset_dict["height"])
-        if not image_wh == expected_wh:
-            raise SizeMismatchError(
-                "Mismatched image shape{}, got {}, expect {}.".format(
-                    " for image " + dataset_dict["file_name"]
-                    if "file_name" in dataset_dict
-                    else "",
-                    image_wh,
-                    expected_wh,
-                )
-                + " Please check the width/height in your annotation."
-            )
+  #  if "width" in dataset_dict or "height" in dataset_dict:
+   #     image_wh = (image.shape[1], image.shape[0])
+    #    expected_wh = (dataset_dict["width"], dataset_dict["height"])
+     #   if not image_wh == expected_wh:
+      #      raise SizeMismatchError(
+       #         "Mismatched image shape{}, got {}, expect {}.".format(
+        #            " for image " + dataset_dict["file_name"]
+         #           if "file_name" in dataset_dict
+          #          else "",
+           #         image_wh,
+            #        expected_wh,
+             #   )
+              #  + " Please check the width/height in your annotation."
+            #)
 
     # To ensure bbox always remap to original image size
     if "width" not in dataset_dict:
